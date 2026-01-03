6cffdbccbabd9e7eacea74ca064d2a3be736a3e7
Yushi <yushari@seznam.cz>
2026-01-03 13:50:58 +0100

    added functionality for the dark mode higher brighterness

--- docs/script.js
+++ docs/script.js
@@ -3,3 +3,22 @@ const toggleBtn = document.getElementById('toggle__button');
 toggleBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-theme');
 });
+
+
+const filter = () => {
+  const isDarkTheme = document.documentElement.classList.contains('dark-theme');
+  const socialContents = document.querySelectorAll('.social__content, .overview__content');
+
+  socialContents.forEach(content => {
+    if (isDarkTheme) {
+      content.classList.add('dark-filter');
+    } else {
+      content.classList.remove('dark-filter');
+    }
+  });
+};
+
+toggleBtn.addEventListener('click', filter);
+
+// Initial filter application
+filter();
